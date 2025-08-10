885353ed972cbc7ff4746dcafa711960e2158d94
Sheharyar Daym <147532749+SheharyarDaym@users.noreply.github.com>
2025-08-11 00:48:51 +0500

    Update vite.config.js

--- Front-app/vite.config.js
+++ Front-app/vite.config.js
@@ -1,8 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [tailwindcss(),react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
+  base: '/', // Important for Vercel SPA routing
+  build: {
+    outDir: 'dist'
+  }
 })
